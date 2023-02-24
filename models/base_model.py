faf8f5c79918ee42f833d8e23d2ccd808c70e9d9
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-02-24 11:17:42 -0800

    i think i fixed it

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,51 @@
 #!/usr/bin/python3
-"""a basemodel that defines all common attrs/models for other classes"""
-
-import uuid
-from datetime import datetime
+"""Defines the BaseModel class."""
 import models
+from uuid import uuid4
+from datetime import datetime
 
 
-class BaseModel():
-    """this is the class that defines common stuff"""
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """ a constructor instance that prefarably uses of kwargs if !empty"""
+        """Initialize a new BaseModel.
 
-        if kwargs:
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, time)
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    setattr(self, key, value)
-
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
-        def __str__(self):
-            """this returns a string representation of the isinstance"""
-            return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                          self.__dict__))
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
-        def save(self):
-            """this updates and saves the public instance attribute
-            updated_at with the latest datetime"""
-            self.updated_at = datetime.now()
-            models.storage.save()
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
 
-        def to_dict(self):
-            """returns a serialized dictionary containing
-            the keys / value of the instance"""
-            dict_rep = self.__dict__.copy()
-            dict_rep.update({'__class__': self.__class__.__name__})
-            dict_rep.update({'created_at': self.created_at.isoformat()})
-            dict_rep.update({'updated_at': self.updated_at.isoformat()})
-            return (dict_rep)
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
--- web_static/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# this is a shell script that helps me simplify adding and pushing to github
-# Usage:
-#	echo "whatever commit message you want to use" | ./test.sh
-# your commit message is read as an input and then all files are added, commited and pushed to your current remote branch on the repository
-read commit_message
-git add .
-git commit -m "$commit_message"
-git push
